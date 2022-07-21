01da11500e249bbb8288d9c8fc034bfe06e6396e
luis <bluerizabiz@gmail.com>
2022-07-20 21:20:20 -0500

    generateMarkDown.js created

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,30 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Table of Contents
+-[Description](#description)
+-[Installation](#installation)
+-[Usage](#usage)
+-[License](#license)
+-[Contributions](#contributions)
+-[Tests](#tests)
+-[Questions](#questions)
 
+## Descriptions
+${data.descriptions}
+## Installations
+${data.installations}
+## Usage
+${data.usage}
+## License 
+![Github license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+## Contributions 
+${data.contributions}
+## Tests
+${data.tests}
+## Questions
+-GitHub: [${data.github}(https://github.com/${data.github})]
+-Email: [${data.email}](mailto:example@example.com)
 `;
 }
 
